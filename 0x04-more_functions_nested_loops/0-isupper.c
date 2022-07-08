409e1e47191255e0490080c30d058732d37022bc
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-07-08 10:26:24 -0700

    My 0 task commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,8 +6,12 @@
 */
 int _isupper(int c)                                                                   
 {                                                                                     
-if (c >= 'A' && c <= 'Z')                                                     
-return (1);                                                           
-else                                                                                 
+if (c >= 'A' && c <= 'Z')
+{	
+return (1);
+}
+else
+{
 return (0);                                                         
-}            
+}
+}

219518884b796a9d799ef89b9f365c6f31c7a6b0
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-07-08 13:47:25 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,5 @@
-#include "main.h"                                                                     
+#include "main.h"
+#include <stdio.h>
 /**                                                                                  
  *_isupper - Checks for uppercase characters.                                     
  *@c: The character to be checked                                                 

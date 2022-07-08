ccbcfbb0030315bb7775959c31f23d84f1cd3d94
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-07-08 13:44:34 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,10 +4,7 @@
  *@c: The character to be checked                                                 
  *Return: 1 if character is uppercase, 0 otherwise.                                
  */
-int _isupper(int c)                                                                   
-/**
- *int initialization
-*/	
+int _isupper(int c)                                                                   	
 {                                                                                     
 if (c >= 'A' && c <= 'Z')
 {	

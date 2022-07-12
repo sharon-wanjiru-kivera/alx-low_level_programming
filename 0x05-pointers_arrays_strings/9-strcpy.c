b3fa9660e6a282825462e9465b47ff2adfa548c0
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-07-12 05:46:01 -0700

    add file

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcpy - check the code
+ * @dest: pointer char type
+ * @src: pointer char type
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i;
+for (i = 0; *src != '\0'; i++)
+{
+dest[i] = *src;
+src++;
+}
+dest[i++] = *src;
+return (dest);
+}

530e25b29a27d40ba4c0cd5aad247be7755516db
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-07-14 14:09:06 -0700

    add file

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,25 +2,40 @@
 #include <stdio.h>
 /**
  * rot13 - check the code
- * @s: is a pointer type char
- * Return: the string s
+ * @s: is the string
+ * Return: the encoded  string s
  */
 char *rot13(char *s)
 {
-int a = 0;
-while (s[a])
+int index1, index2;
+char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+	              'G', 'H', 'I', 'J', 'K', 'L',
+		      'M', 'N', 'O', 'P', 'Q', 'R',
+		      'S', 'T', 'U', 'V', 'W', 'X',
+		      'Y', 'Z', 'a', 'b', 'c', 'd',
+		      'e', 'f', 'g', 'h', 'i', 'j',
+		      'k', 'l', 'm', 'n', 'o', 'p',
+		      'q', 'r', 's', 't', 'u', 'v',
+		      'w', 'x', 'y', 'z'};
+char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+	              'T', 'U', 'V', 'W', 'X', 'Y',
+		      'Z', 'A', 'B', 'C', 'D', 'E',
+		      'F', 'G', 'H', 'I', 'J', 'K',
+		      'L', 'M', 'n', 'o', 'p', 'q',
+		      'r', 's', 't', 'u', 'v', 'w',
+		      'x', 'y', 'z', 'a', 'b', 'c',
+		      'd', 'e', 'f', 'g', 'h', 'i',
+                      'j', 'k', 'l', 'm'};
+while (str[++index1])
+}
+for (index2 = 0; index2 < 52; index2++)
 {
-while ((s[a] >= 'a' && s[a] <= 'z') || (s[a] >= 'A' && s[a] <= 'Z'))
+if (s[index1] == alphabet[index2])
 {
-if ((s[a] > 'm' && s[a] <= 'z') || (s[a] > 'M' && s[a] <= 'Z'))
-{
-s[a] -= 13;
-break;
-{
-s[a] += 13;
+s[index1] = rot13key[index2];
 break;
 }
-a++;
+}
 }
 return (s);
 }

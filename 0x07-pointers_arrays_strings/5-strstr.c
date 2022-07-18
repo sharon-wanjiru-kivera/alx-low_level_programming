e0c3c4eff1ef9907418db63049ed93002b444468
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-07-18 06:06:10 -0700

    add file

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,58 +1,59 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *  _strstr - function 
- *  @s: first string
- *  @accept: second string
- *  Return: unsigned int
+ * _strstr - function 
+ * @s: first string
+ * @accept: second string
+ * Return: unsigned int
  */
 char *_strstr(char *haystack, char *needle)
+
 {
-unsigned int i = 0, j = 0, stop1 = 1, mtp = 1, len = 0, finish = 0;
-char *tmp, *src, *dst;
-while (*(haystack + i))
-{
-*(src + i) = *(haystack + i);
-i++;
-}
-while (*(needle + j))
-{
-dst[j] = needle[j];
-j++;
-}
-while (needle[len])
-len++;
-printf("lenght of needle %d \n \n", len);
-for (i = 0; *(src + i); i++)
-{
-stop1 = 0;
-for (j = 0; j < len; j++)
-{
-if (src[i] == dst[j])
-{
-mtp++;
-stop1 *= 1;
-}
-else
-stop1 *= 0;
-if (stop1 == 0)
-break;
-else
-if (mtp == len)
-{
-finish = 1;
-tmp = src + j;
-}
-printf("i: %d , j: %d, mtp: %d, stop: %d, finish: %d \n", i, j, mtp ,stop1, finish);
-}
-if(stop1 == 0)
-break;
-if (j > len - i)
-break;
-if ( finish == 1)
-{
-break;
-}
-}
-return (tmp);
+
+		int a = 0, b = 0;
+
+
+
+			while (haystack[a])
+
+					{
+
+								while (needle[b])
+
+											{
+
+															if (haystack[a + b] != needle[b])
+
+																			{
+
+																								break;
+
+																											}
+
+
+
+																		b++;
+
+																				}
+
+
+
+										if (needle[b] == '\0')
+
+													{
+
+																	return (haystack + a);
+
+																			}
+
+
+
+												a++;
+
+													}
+
+
+
+				return ('\0');
+
 }

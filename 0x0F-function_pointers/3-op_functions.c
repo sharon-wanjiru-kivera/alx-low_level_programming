369cb9894a12839077c32e8cc3f9cfd42444ff7d
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-08-05 19:44:37 -0700

    add file

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,44 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * op_add - 5 functions
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - 5 functions
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function
+ * @a: input
+ * @b: input
+ *  Return: 0
+ */
+int op_div(int a, int b)
+{
+
+	return (a / b);
+}
+/**
+ * op_mod - function
+ * @a: input
+ * @b: input
+ * Return: 0
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

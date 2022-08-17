313c40c4adc4b0507fa9d97d00401a85080b887d
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-08-17 16:54:30 -0700

    add file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -7,12 +7,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-size_t nodes = 0;
-while (h)
-{
-nodes++;
-printf("%d\n", h->n);
-h = h->next;
-}
-return (nodes);
+	size_t nodes = 0;
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (nodes);
 }

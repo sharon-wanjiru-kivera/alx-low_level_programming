c51e5f474e655f3a628436281cfb05f9d899a3b4
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-08-17 14:06:40 -0700

    add file

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,11 +1,13 @@
 #include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 /**
  * listint_len - Returns the number of elements
  * @h: A pointer to the head of the listint_t list
  * Return: The number of elements in the listint_t list.
  */
-size_t listint_len(const listint_t *h)
+size_t listint_len(const listint_t *h);
 {
 	size_t nodes = 0;
 	while (h)
--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,3 +1,6 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 #include "lists.h"
 /**
  * add_nodeint - Adds a new node at the beginning
@@ -6,7 +9,7 @@
  * Return: If the function fails - NULL
  Otherwise - the address of the new element
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t *add_nodeint(listint_t **head, const int n);
 {
 	listint_t *new;
 	new = malloc(sizeof(listint_t));

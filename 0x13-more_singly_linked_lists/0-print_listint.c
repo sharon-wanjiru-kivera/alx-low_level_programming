bd04c360b66a5537f9e4aacc93dd192d91082b8f
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-08-17 17:00:01 -0700

    add file

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,7 +1,9 @@
 #include "lists.h"
 #include <stdio.h>
+
 /**
- * print_listint - Prints all the element of a listint_t list.
+ * print_listint - Prints all the element of a
+ *                  listint_t list.
  * @h: A pointer to the head of the list_t list.
  * Return: The number of nodes in the list_t list.
  */

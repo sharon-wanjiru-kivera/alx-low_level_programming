93a837dc99c1747b617150ca78a8006741db0aa0
sharon-wanjiru-kivera <kiberasharon496@gmail.com>
2022-08-22 18:47:02 -0700

    add file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,8 +4,8 @@
 /**
  * create_file - function that creates a file.
  * Prototype - int create_file(const char *filename, char *text_content);
- * @filename - variable pointer
- * @text_content: content file
+ * @filename -  name of the file to create
+ * @text_content - NULLterminated string to write to the file
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
